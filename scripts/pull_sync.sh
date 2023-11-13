b7ba5979c392a8f7c71edf6c58e1ab0f59623c6b
kefi550 <ytiryrstd550@gmail.com>
2023-11-14 01:15:04 +0900

    push, pullコマンド

--- scripts/pull_sync.sh
+++ scripts/pull_sync.sh
@@ -4,10 +4,13 @@ set -u
 
 LOCAL_PATH="../../isucon9-q"
 
-rsync -avr --exclude='node_modules' isu3:~/isucari/webapp/nodejs/ "${LOCAL_PATH}/isucari/webapp/nodejs/"
-rsync -avr --exclude='initial.sql' isu3:~/isucari/webapp/sql/ "${LOCAL_PATH}/isucari/webapp/sql/"
+rsync -avr --exclude='node_modules' isu1:~/isucari/webapp/nodejs/ "${LOCAL_PATH}/isucari/webapp/nodejs/"
+rsync -avr --exclude='initial.sql' isu1:~/isucari/webapp/sql/ "${LOCAL_PATH}/isucari/webapp/sql/"
+
 rsync -avr --exclude='' isu1:/etc/nginx/ "${LOCAL_PATH}/nginx/isu1/"
 rsync -avr --exclude='' isu2:/etc/nginx/ "${LOCAL_PATH}/nginx/isu2/"
 rsync -avr --exclude='' isu3:/etc/nginx/ "${LOCAL_PATH}/nginx/isu3/"
-rsync -avr --exclude='' isu2:/etc/mysql/ "${LOCAL_PATH}/mysql/"
+rsync -avr --exclude='' isu1:/etc/mysql/ "${LOCAL_PATH}/mysql/isu1"
+rsync -avr --exclude='' isu2:/etc/mysql/ "${LOCAL_PATH}/mysql/isu2"
+rsync -avr --exclude='' isu3:/etc/mysql/ "${LOCAL_PATH}/mysql/isu3"
   
--- scripts/push_sync.sh
+++ scripts/push_sync.sh
@@ -6,16 +6,18 @@ LOCAL_PATH="../../isucon9-q"
 
 rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu1:~/isucari/webapp/nodejs/ 
 ssh isu1 sudo systemctl restart isucari.nodejs.service
-rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu2:~/isucari/webapp/nodejs/ 
-# ssh isu2 sudo systemctl restart isucari.nodejs.service
+rsync -vr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu2:~/isucari/webapp/nodejs/ 
+ssh isu2 sudo systemctl restart isucari.nodejs.service
 rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/nodejs/" isu3:~/isucari/webapp/nodejs/ 
 ssh isu3 sudo systemctl restart isucari.nodejs.service
-rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/sql/" isu1:~/isucari/webapp/sql/ 
-rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/sql/" isu2:~/isucari/webapp/sql/ 
-rsync -avr --exclude='node_modules' "${LOCAL_PATH}/isucari/webapp/sql/" isu3:~/isucari/webapp/sql/ 
-rsync -avr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu1:~/isucari/webapp/sql/
-rsync -avr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu2:~/isucari/webapp/sql/
-rsync -avr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu3:~/isucari/webapp/sql/
-# rsync -avr --exclude='' "${LOCAL_PATH}/nginx/" isu1:/etc/nginx/
-# rsync -avr --exclude='' "${LOCAL_PATH}/mysql/" isu2:/etc/mysql/
+rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu1:~/isucari/webapp/sql/
+rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu2:~/isucari/webapp/sql/
+rsync -vr --exclude='initial.sql' "${LOCAL_PATH}/isucari/webapp/sql/" isu3:~/isucari/webapp/sql/
+
+rsync -vr --exclude='' "${LOCAL_PATH}/nginx/isu1/" isu1:/etc/nginx/
+rsync -vr --exclude='' "${LOCAL_PATH}/nginx/isu2/" isu2:/etc/nginx/
+rsync -vr --exclude='' "${LOCAL_PATH}/nginx/isu3/" isu3:/etc/nginx/
+rsync -vr --exclude='' "${LOCAL_PATH}/mysql/isu1/" isu1:/etc/mysql/
+rsync -vr --exclude='' "${LOCAL_PATH}/mysql/isu2/" isu2:/etc/mysql/
+rsync -vr --exclude='' "${LOCAL_PATH}/mysql/isu3/" isu3:/etc/mysql/
   
